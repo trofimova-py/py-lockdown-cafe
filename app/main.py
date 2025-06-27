5b3c0de338d741e806b35bbf79f763d9ecd309aa
Yuliia Trofimova <julia1golubenko@gmail.com>
2025-06-27 18:59:02 +0300

    Fix logic in go_to_cafe to check all friends before deciding

--- app/main.py
+++ app/main.py
@@ -5,16 +5,18 @@ from app.errors import VaccineError, NotWearingMaskError
 
 def go_to_cafe(friends: List[Dict[str, Any]], cafe: Cafe) -> str:
     masks_to_buy: int = 0
+    someone_unvaccinated: bool = False
 
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
-            return "All friends should be vaccinated"
+            someone_unvaccinated = True
         except NotWearingMaskError:
             masks_to_buy += 1
 
+    if someone_unvaccinated:
+        return "All friends should be vaccinated"
     if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
-
     return f"Friends can go to {cafe.name}"
